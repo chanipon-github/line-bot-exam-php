471839ddd4bf77b2a8fc8f47f0ae1da46c575a8d
chanipon-github <chanipon_thipkul@hotmail.com>
2019-05-16 16:57:52 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,8 +3,7 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
-
+$access_token = 'aURTpXHSHZ1Yb5dVKSxnjvTcbUl8sA8HxLNrlM/58q27Mf0ql4MS2HqsVLOUHcxxRC+gYDdW7sCA+JOXSbXVxoZuBjSYQzKM3HQkFD+ADpavXSk+kEcn5kWByGNDm5617+pmbqGhmu11dtt7qI2TpQdB04t89/1O/w1cDnyilFU='
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
