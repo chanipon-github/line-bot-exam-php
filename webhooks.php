48e6470387f616c9e9f5259c3e02eaaabe26e9f6
chanipon-github <chanipon_thipkul@hotmail.com>
2019-05-16 17:09:18 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,7 +17,7 @@ if (!is_null($events['events'])) {
 			// Get text sent
 			$text = $event['source']['userId'];
 			// Get replyToken
-			$replyToken = $event['replyToken'];
+			$replyToken = $event['ddddddd'];
 
 			// Build message to reply back
 			$messages = [
@@ -28,7 +28,7 @@ if (!is_null($events['events'])) {
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
-				'replyToken' => $replyToken,
+				'ddddddd' => $replyToken,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
