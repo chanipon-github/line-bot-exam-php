f28265b217a1577b6dfa63ed3989bece3abd8a4b
chanipon-github <chanipon_thipkul@hotmail.com>
2019-05-16 17:51:48 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -9,21 +9,6 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
-	// Loop through each event
-	foreach ($events['events'] as $event) {
-		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			// Get text sent
-			$text = $event['source']['userId'];
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
 
 			if($message == "สวัสดี"){
 				$arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
