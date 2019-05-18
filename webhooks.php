45f2c52dc6c5328f9d83c2d255270e9729227a95
chanipon-github <chanipon_thipkul@hotmail.com>
2019-05-18 13:52:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -66,14 +66,16 @@ $access_token = '6TvBLa/XIptJXxGnGyjbueq2qsxnT+asIMk+Qx25KhJJ23H6ARgKZE5AxxT+HGW
 		
     } 
    
-    else if($message == "1-10"){
+    $id = $arrayJson['events'][0]['source']['userId'];
+
+    else  if($message == "1-10"){
         for($i=1;$i<=10;$i++){
-            $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
            $arrayPostData['to'] = $id;
            $arrayPostData['messages'][0]['type'] = "text";
            $arrayPostData['messages'][0]['text'] = $i;
            pushMsg($arrayHeader,$arrayPostData);
         }
+     }
 
  
 
