52df251cc06c5c80216f7e37083a787cb408f633
chanipon-github <chanipon_thipkul@hotmail.com>
2019-05-20 17:52:38 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -1,178 +1,24 @@
 
 <?php
-$access_token = '6TvBLa/XIptJXxGnGyjbueq2qsxnT+asIMk+Qx25KhJJ23H6ARgKZE5AxxT+HGW3RC+gYDdW7sCA+JOXSbXVxoZuBjSYQzKM3HQkFD+ADpbyT9KL0Rofdpwbkj3M1cd8lOmY/D7CpSC36kKspN0ilQdB04t89/1O/w1cDnyilFU=';
-    
-    $content = file_get_contents('php://input');
-    $arrayJson = json_decode($content, true); 
-    
-    $arrayHeader = array();
-    $arrayHeader[] = "Content-Type: application/json";
-    $arrayHeader[] = "Authorization: Bearer {$access_token}";
-
-
-    if(!is_null($is_message)){
-        switch ($typeMessage){
-            case 'text':
-                $userMessage = strtolower($userMessage); // แปลงเป็นตัวเล็ก สำหรับทดสอบ
-                switch ($userMessage) {
-                    case "t":
-                        $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
-                        $replyData = new TextMessageBuilder($textReplyMessage);
-                        break;
-                    case "i":
-                        $picFullSize = 'https://storage.googleapis.com/koratstartup-files/2018/08/3af81a2e-28378108_817627798437743_1479477558157879719_n.jpg';
-                        $picThumbnail = 'https://storage.googleapis.com/koratstartup-files/2018/08/3af81a2e-28378108_817627798437743_1479477558157879719_n.jpg/240';
-                        $replyData = new ImageMessageBuilder($picFullSize,$picThumbnail);
-                        break;
-                    case "v":
-                        $picThumbnail = 'https://storage.googleapis.com/koratstartup-files/2018/08/3af81a2e-28378108_817627798437743_1479477558157879719_n.jpg/240';
-                        $videoUrl = "https://www.youtube.com/watch?v=OxWw8aBcgtQ";             
-                        $replyData = new VideoMessageBuilder($videoUrl,$picThumbnail);
-                        break;
-                    case "l":
-                        $placeName = "ที่ตั้งร้าน";
-                        $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
-                        $latitude = 13.780401863217657;
-                        $longitude = 100.61141967773438;
-                        $replyData = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);              
-                        break;
-                    case "m":
-                        $textReplyMessage = "Bot ตอบกลับคุณเป็นข้อความ";
-                        $textMessage = new TextMessageBuilder($textReplyMessage);
-                                         
-                        $picFullSize = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower';
-                        $picThumbnail = 'https://www.mywebsite.com/imgsrc/photos/f/simpleflower/240';
-                        $imageMessage = new ImageMessageBuilder($picFullSize,$picThumbnail);
-                                         
-                        $placeName = "ที่ตั้งร้าน";
-                        $placeAddress = "แขวง พลับพลา เขต วังทองหลาง กรุงเทพมหานคร ประเทศไทย";
-                        $latitude = 14.974498;
-                        $longitude = 102.110534;
-                        $locationMessage = new LocationMessageBuilder($placeName, $placeAddress, $latitude ,$longitude);        
-     
-                        $multiMessage =     new MultiMessageBuilder;
-                        $multiMessage->add($textMessage);
-                        $multiMessage->add($imageMessage);
-                        $multiMessage->add($locationMessage);
-                        $replyData = $multiMessage;                                     
-                        break;                  
-                    case "s":
-                        $stickerID = 22;
-                        $packageID = 2;
-                        $replyData = new StickerMessageBuilder($packageID,$stickerID);
-                        break;      
-                    case "im":
-                        $imageMapUrl = 'https://brandinside.asia/wp-content/uploads/2017/08/class-drive.jpg';
-                        $replyData = new ImagemapMessageBuilder(
-                            $imageMapUrl,
-                            'This is Title',
-                            new BaseSizeBuilder(699,1040),
-                            array(
-                                new ImagemapMessageActionBuilder(
-                                    'test image map',
-                                    new AreaBuilder(0,0,520,699)
-                                    ),
-                                new ImagemapUriActionBuilder(
-                                    'http://www.ninenik.com',
-                                    new AreaBuilder(520,0,520,699)
-                                    )
-                            )); 
-                        break;          
-                    case "tm":
-                        $replyData = new TemplateMessageBuilder('Confirm Template',
-                            new ConfirmTemplateBuilder(
-                                    'Confirm template builder',
-                                    array(
-                                        new MessageTemplateActionBuilder(
-                                            'Yes',
-                                            'Text Yes'
-                                        ),
-                                        new MessageTemplateActionBuilder(
-                                            'No',
-                                            'Text NO'
-                                        )
-                                    )
-                            )
-                        );
-                        break;          
-                    case "t_b":
-                        // กำหนด action 4 ปุ่ม 4 ประเภท
-                        $actionBuilder = array(
-                            new MessageTemplateActionBuilder(
-                                'Message Template',// ข้อความแสดงในปุ่ม
-                                'This is Text' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                            ),
-                            new UriTemplateActionBuilder(
-                                'Uri Template', // ข้อความแสดงในปุ่ม
-                                'https://www.wongnai.com/chains/classcafe'
-                            ),
-                            new DatetimePickerTemplateActionBuilder(
-                                'Datetime Picker', // ข้อความแสดงในปุ่ม
-                                http_build_query(array(
-                                    'action'=>'reservation',
-                                    'person'=>5
-                                )), // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-                                'datetime', // date | time | datetime รูปแบบข้อมูลที่จะส่ง ในที่นี้ใช้ datatime
-                                substr_replace(date("Y-m-d H:i"),'T',10,1), // วันที่ เวลา ค่าเริ่มต้นที่ถูกเลือก
-                                substr_replace(date("Y-m-d H:i",strtotime("+5 day")),'T',10,1), //วันที่ เวลา มากสุดที่เลือกได้
-                                substr_replace(date("Y-m-d H:i"),'T',10,1) //วันที่ เวลา น้อยสุดที่เลือกได้
-                            ),      
-                            new PostbackTemplateActionBuilder(
-                                'Postback', // ข้อความแสดงในปุ่ม
-                                http_build_query(array(
-                                    'action'=>'buy',
-                                    'item'=>100
-                                )) // ข้อมูลที่จะส่งไปใน webhook ผ่าน postback event
-    //                          'Postback Text'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                            ),      
-                        );
-                        $imageUrl = 'https://brandinside.asia/wp-content/uploads/2017/08/class-drive.jpg';
-                        $replyData = new TemplateMessageBuilder('Button Template',
-                            new ButtonTemplateBuilder(
-                                    'button template builder', // กำหนดหัวเรื่อง
-                                    'Please select', // กำหนดรายละเอียด
-                                    $imageUrl, // กำหนด url รุปภาพ
-                                    $actionBuilder  // กำหนด action object
-                            )
-                        );              
-                        break;      
-                    case "t_f":
-                        $replyData = new TemplateMessageBuilder('Confirm Template',
-                            new ConfirmTemplateBuilder(
-                                    'Confirm template builder', // ข้อความแนะนหรือบอกวิธีการ หรือคำอธิบาย
-                                    array(
-                                        new MessageTemplateActionBuilder(
-                                            'Yes', // ข้อความสำหรับปุ่มแรก
-                                            'YES'  // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                                        ),
-                                        new MessageTemplateActionBuilder(
-                                            'No', // ข้อความสำหรับปุ่มแรก
-                                            'NO' // ข้อความที่จะแสดงฝั่งผู้ใช้ เมื่อคลิกเลือก
-                                        )
-                                    )
-                            )
-                        );
-                        break;      
-                                                                                                                                                                                           
-                    default:
-                        $textReplyMessage = " คุณไม่ได้พิมพ์ ค่า ตามที่กำหนด";
-                        $replyData = new TextMessageBuilder($textReplyMessage);         
-                        break;                                      
-                }
-                break;
-            default:
-                $textReplyMessage = json_encode($events);
-                $replyData = new TextMessageBuilder($textReplyMessage);         
-                break;  
-        }
+    define('LINE_API',"https://notify-api.line.me/api/notify");
+    $token = "6TvBLa/XIptJXxGnGyjbueq2qsxnT+asIMk+Qx25KhJJ23H6ARgKZE5AxxT+HGW3RC+gYDdW7sCA+JOXSbXVxoZuBjSYQzKM3HQkFD+ADpbyT9KL0Rofdpwbkj3M1cd8lOmY/D7CpSC36kKspN0ilQdB04t89/1O/w1cDnyilFU="; 
+    $str = "มีรายการสั่งซื้อ กรุณาเช็ค อีเมลล์ครับ"; 
+    $res = notify_message($str,$token);
+    print_r($res);
+    function notify_message($message,$token){
+    $queryData = array('message' => $message);
+    $queryData = http_build_query($queryData,'','&');
+    $headerOptions = array( 
+    'http'=>array(
+    'method'=>'POST',
+    'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+    ."Authorization: Bearer ".$token."\r\n"
+    ."Content-Length: ".strlen($queryData)."\r\n",
+    'content' => $queryData
+    ),
+    );
+    $context = stream_context_create($headerOptions);
+    $result = file_get_contents(LINE_API,FALSE,$context);
+    return $res;
     }
-}
-$response = $bot->replyMessage($replyToken,$replyData);
-if ($response->isSucceeded()) {
-    echo 'Succeeded!';
-    return;
-}
- 
-// Failed
-echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
 ?>
